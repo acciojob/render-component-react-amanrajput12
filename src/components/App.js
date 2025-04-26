930d796ed2b0d4b1b40f60ae351df2b14a1ac735
Aman Rajput <rajputaman724857@gmail.com>
2025-04-26 14:45:37 +0530

    create comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Greating from "./Greeting.js";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Greating/>
+    </div>
   )
 }
 
